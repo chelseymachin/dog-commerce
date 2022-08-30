134c76e52879ccc54a6937d36bfba870ac48ff28
chelseymachin <chelseymachin@gmail.com>
2022-08-30 10:54:34 -0700

    updated pinia store and related components for visuals to use temperament filter instead of heat

--- components/ProductFilters.vue
+++ components/ProductFilters.vue
@@ -1,9 +1,26 @@
 <script setup>
+import { debouncedWatch, refDebounced } from "@vueuse/core";
+const { fetchProducts } = useProductStore();
+const router = useRouter();
+const loading = ref(false);
+const loadingDebounced = refDebounced(loading, 500);
 const productStore = useProductStore();
 const filters = computed(() => productStore.filters);
+
+debouncedWatch(
+    filters,
+    async () => {
+      loading.value = true;
+      router.push({ query: filters.value });
+      await fetchProducts();
+      loading.value = false;
+    },
+    { deep: true, debounce: 200 }
+);
 </script>
 <template>
   <div class="filters-wrapper flex gap-2 items-center">
+    <AppSpinner style="transform: translateY(15px)" v-if="loadingDebounced" />
     <div class="form-control">
       <label class="label" for="search">
         <span class="label-text">Search</span>
@@ -16,18 +33,20 @@ const filters = computed(() => productStore.filters);
       />
     </div>
     <div class="form-control w-full max-w-xs">
-      <label class="label" for="filterHeat">
-        <span class="label-text">Filter By Heat</span>
+      <label class="label" for="filterTemperament">
+        <span class="label-text">Filter By Temperament</span>
       </label>
       <select
-        id="filterHeat"
+        id="filterTemperament"
         class="select select-bordered"
-        v-model="filters[`fields.heatLevel`]"
+        v-model="filters[`fields.temperament`]"
       >
         <option value="">All</option>
-        <option value="Mild">Mild</option>
-        <option value="Medium">Medium</option>
-        <option value="Hot">Hot</option>
+        <option value="Active">Active</option>
+        <option value="Good">Good</option>
+        <option value="Greedy">Greedy</option>
+        <option value="Lazy">Lazy</option>
+        <option value="Mischievous">Mischievous</option>
       </select>
     </div>
     <div class="form-control w-full max-w-xs">
@@ -40,8 +59,6 @@ const filters = computed(() => productStore.filters);
         id="orderBy"
       >
         <option value="">None</option>
-        <option value="-fields.heatLevel">Heat (Mild First)</option>
-        <option value="fields.heatLevel">Heat (Hot First)</option>
         <option value="fields.price">Price (Low to High)</option>
         <option value="-fields.price">Price (High to Low)</option>
       </select>
